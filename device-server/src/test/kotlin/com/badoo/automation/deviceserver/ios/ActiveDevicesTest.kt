e3bd521d5390aca109dd79fa4f870e13c4725ad5
Nikolai Abalov <n.abalov@corp.badoo.com>
2018-06-06 11:16:51 +0100

    Fix list method crashing on device disconnection

---
+++ device-server/src/test/kotlin/com/badoo/automation/deviceserver/ios/ActiveDevicesTest.kt
@@ -0,0 +1,32 @@
+package com.badoo.automation.deviceserver.ios
+
+import com.badoo.automation.deviceserver.deviceDTOStub
+import com.badoo.automation.deviceserver.host.ISimulatorsNode
+import com.badoo.automation.deviceserver.host.management.errors.DeviceNotFoundException
+import com.badoo.automation.deviceserver.mockThis
+import com.nhaarman.mockito_kotlin.whenever
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.time.Duration
+
+class ActiveDevicesTest {
+
+    @Test
+    fun deviceListShouldIgnoreDisconnectedDevices() {
+        val activeDevices = ActiveDevices()
+        val node = mockThis<ISimulatorsNode>()
+
+        whenever(node.getDeviceDTO("ref1")).thenReturn(deviceDTOStub("ref1"))
+        whenever(node.getDeviceDTO("ref2")).thenThrow(DeviceNotFoundException(""))
+        whenever(node.getDeviceDTO("ref3")).thenReturn(deviceDTOStub("ref3"))
+
+        activeDevices.registerDevice("ref1", node, Duration.ZERO, null)
+        activeDevices.registerDevice("ref2", node, Duration.ZERO, null)
+        activeDevices.registerDevice("ref3", node, Duration.ZERO, null)
+
+        val list = activeDevices.deviceList()
+
+        assertEquals(listOf("ref1", "ref3"), list.map { it.ref }.sorted())
+        assertEquals(2, activeDevices.deviceRefs().size)
+    }
+}
