5ab9cc1f1e792f226de7d05f0d922d3c8a70f544
Vyacheslav Frolov <vyacheslav.frolov@corp.badoo.com>
2019-03-21 20:29:42 +0000

    Use lightweight way to check simulator status. Make 'get simulator status' idempotent.

--- device-server/src/main/kotlin/com/badoo/automation/deviceserver/data/SimulatorStatus.kt
+++ device-server/src/main/kotlin/com/badoo/automation/deviceserver/data/SimulatorStatus.kt
@@ -1,9 +1,10 @@
 package com.badoo.automation.deviceserver.data
 
 data class SimulatorStatus(
-        var isReady: Boolean = false,
         var wdaStatus: Boolean = false,
         var fbsimctlStatus: Boolean = false,
         @Volatile var wdaStatusRetries: Int = 0,
         @Volatile var fbsimctlStatusRetries: Int = 0
-)
+) {
+    val isReady: Boolean get() = wdaStatus && fbsimctlStatus
+}
