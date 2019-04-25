e6da467a6dde46b923d036fd5f852af9506cdbf2
Vyacheslav Frolov <vyacheslav.frolov@corp.badoo.com>
2019-04-25 09:22:46 +0100

    Exclude irrelevant fields from CommandResult toString

--- device-server/src/main/kotlin/com/badoo/automation/deviceserver/command/CommandResult.kt
+++ device-server/src/main/kotlin/com/badoo/automation/deviceserver/command/CommandResult.kt
@@ -1,5 +1,9 @@
 package com.badoo.automation.deviceserver.command
 
+import java.lang.StringBuilder
+
+private const val MAX_CHARS = 1024
+
 data class CommandResult(
     // FIXME: Separate binary and string results for "capture" and "process open" executors
     val stdOut: String,
@@ -8,4 +12,18 @@ data class CommandResult(
     val exitCode: Int,
     val isSuccess: Boolean = exitCode == 0,
     val cmd: List<String> = listOf()
-)
+) {
+    override fun toString(): String {
+        val sb = StringBuilder(javaClass.simpleName)
+        sb.append("(")
+        sb.append("cmd=$cmd")
+        sb.append(", ")
+        sb.append("exitCode=$exitCode")
+        sb.append(", ")
+        sb.append("stdOut=${stdOut.take(MAX_CHARS)}")
+        sb.append(", ")
+        sb.append("stdErr=${stdErr.take(MAX_CHARS)}")
+        sb.append(")")
+        return sb.toString()
+    }
+}
