02b35f1c4ee7aec76b584bc6ed3832807c8232f6
Vyacheslav Frolov <vyacheslav.frolov@corp.badoo.com>
2019-04-25 09:22:46 +0100

    Exclude irrelevant fields from CommandResult toString

---
+++ device-server/src/test/kotlin/com/badoo/automation/deviceserver/command/CommandResultTest.kt
@@ -0,0 +1,21 @@
+package com.badoo.automation.deviceserver.command
+
+import org.junit.Assert
+import org.junit.Test
+
+class CommandResultTest {
+    @Test
+    fun commandResultContainsRelevantFields() {
+        val expectedResult = "CommandResult(cmd=[rm, -rf, /], exitCode=0, stdOut=out, stdErr=err)"
+
+        val actualResult = CommandResult(
+                stdOut = "out",
+                stdErr = "err",
+                stdOutBytes = "out".toByteArray(),
+                exitCode = 0,
+                cmd = listOf("rm", "-rf", "/")
+        ).toString()
+
+        Assert.assertEquals("Wrong result string", expectedResult, actualResult)
+    }
+}
