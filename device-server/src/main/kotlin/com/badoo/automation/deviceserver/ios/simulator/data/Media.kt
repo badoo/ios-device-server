03a15e891fa9b7dce396890bad4ef40be35f8bac
Nikolai Abalov <n.abalov@corp.badoo.com>
2019-04-30 15:43:09 +0100

    Restart com.apple.assetsd on Media reset
    Uploading media files using fbsimctl after reset might fail unless
    com.apple.assetsd is restarted explicitly

--- device-server/src/main/kotlin/com/badoo/automation/deviceserver/ios/simulator/data/Media.kt
+++ device-server/src/main/kotlin/com/badoo/automation/deviceserver/ios/simulator/data/Media.kt
@@ -2,12 +2,11 @@ package com.badoo.automation.deviceserver.ios.simulator.data
 
 import com.badoo.automation.deviceserver.data.UDID
 import com.badoo.automation.deviceserver.host.IRemote
-import java.lang.RuntimeException
 import java.nio.file.Paths
 
 class Media(
     private val remote: IRemote,
-    udid: UDID,
+    private val udid: UDID,
     deviceSetPath: String
 ) {
     private val mediaPath = Paths.get(deviceSetPath, udid, "data", "Media")
@@ -16,12 +15,27 @@ class Media(
         val imagesPath = mediaPath.resolve("DCIM").toString()
         val photoDataPath = mediaPath.resolve("PhotoData/Photos.sqlite").toString()
 
-        val shellCmd = "rm $imagesPath/**/* $photoDataPath*; touch $photoDataPath"
+        val removeCmd = "rm -f $imagesPath/**/* $photoDataPath*; touch $photoDataPath"
 
-        val result = remote.shell(shellCmd)
+        val result = remote.shell(removeCmd)
 
         if (!result.isSuccess) {
             throw RuntimeException("Could not reset Media: $result")
+        }
+
+        // restart assetsd to prevent fbsimctl upload failing with Error Domain=NSCocoaErrorDomain Code=-1 \"(null)\"
+        restartAssetsd()
+    }
+
+    private fun restartAssetsd() {
+        val restartCmd = listOf(
+            "xcrun", "simctl", "spawn", udid, "launchctl", "kickstart", "-k", "-p", "system/com.apple.assetsd"
+        )
+
+        val result = remote.execIgnoringErrors(restartCmd)
+
+        if (!result.isSuccess) {
+            throw RuntimeException("Could not restart assetsd service: $result")
         }
     }
 }
