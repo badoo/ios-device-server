84731bbaef57e99e793a154ef49aa2aae9c63341
Vyacheslav Frolov <vyacheslav.frolov@corp.badoo.com>
2019-12-17 05:50:00 +0000

    HashCode and equals for DesiredCapabilities

--- device-server/src/main/kotlin/com/badoo/automation/deviceserver/data/DesiredCapabilities.kt
+++ device-server/src/main/kotlin/com/badoo/automation/deviceserver/data/DesiredCapabilities.kt
@@ -12,4 +12,28 @@ data class DesiredCapabilities(
 
         @JsonProperty("use_wda")
         val useWda: Boolean = true
-)
+) {
+        override fun equals(other: Any?): Boolean {
+                if (this === other) return true
+                if (javaClass != other?.javaClass) return false
+
+                other as DesiredCapabilities
+
+                if (udid != other.udid) return false
+                if (model != other.model) return false
+                if (os != other.os) return false
+                if (arch != other.arch) return false
+                if (useWda != other.useWda) return false
+
+                return true
+        }
+
+        override fun hashCode(): Int {
+                var result = udid?.hashCode() ?: 0
+                result = 31 * result + (model?.hashCode() ?: 0)
+                result = 31 * result + (os?.hashCode() ?: 0)
+                result = 31 * result + (arch?.hashCode() ?: 0)
+                result = 31 * result + useWda.hashCode()
+                return result
+        }
+}
