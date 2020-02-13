a9718a9bd643dad3a80e9e0236692c00bd9528ee
Vyacheslav Frolov <frolov78@gmail.com>
2020-02-13 15:42:49 +0000

    Add logs to find Simulator process and refactor a bit

--- device-server/src/main/kotlin/com/badoo/automation/deviceserver/ios/simulator/SimulatorProcess.kt
+++ device-server/src/main/kotlin/com/badoo/automation/deviceserver/ios/simulator/SimulatorProcess.kt
@@ -1,40 +1,63 @@
 package com.badoo.automation.deviceserver.ios.simulator
 
+import com.badoo.automation.deviceserver.LogMarkers
+import com.badoo.automation.deviceserver.data.DeviceRef
 import com.badoo.automation.deviceserver.data.UDID
 import com.badoo.automation.deviceserver.host.IRemote
+import net.logstash.logback.marker.MapEntriesAppendingMarker
+import org.slf4j.LoggerFactory
+import org.slf4j.Marker
+import java.util.*
 
 class SimulatorProcess(
     private val remote: IRemote,
-    private val udid: UDID
+    private val udid: UDID,
+    private val deviceRef: DeviceRef
 ) {
-    val mainProcessPid: Int
-        get() {
-            val command = listOf("/usr/bin/pgrep", "-fl", "launchd_sim")
-            val result = remote.execIgnoringErrors(command)
-
-            check(result.isSuccess) {
-                "No launchd_sim process is found for simulator with udid: $udid. Result: $result"
-            }
-
-            val processList = result
-                .stdOut
-                .lines()
-                .filter { it.contains(udid) }
-
-            check(processList.isNotEmpty()) {
-                "No launchd_sim process is found for simulator with udid: $udid. Result: $result"
-            }
-
-            return processList
-                .first()
-                .split(" ")
-                .first()
-                .toInt()
-        }
+    private val logger = LoggerFactory.getLogger(javaClass.simpleName)
+    private val commonLogMarkerDetails = mapOf(
+        LogMarkers.DEVICE_REF to deviceRef,
+        LogMarkers.UDID to udid,
+        LogMarkers.HOSTNAME to remote.publicHostName
+    )
+    private val logMarker: Marker = MapEntriesAppendingMarker(commonLogMarkerDetails)
 
     fun terminateChildProcess(processName: String) {
-        // Sends SIGKILL to process with parent pid $mainProcessPid and name $processName
-        val command = listOf("/usr/bin/pkill", "-9", "-P", "$mainProcessPid", "-f", processName)
-        remote.execIgnoringErrors(command)
+        val mainProcessPid = getSimulatorMainProcessPid()
+
+        // Sends SIGKILL to all processes that:
+        // 1. have parent pid $mainProcessPid
+        // 2. and full command line contains substring $processName
+
+        remote.execIgnoringErrors(listOf("/usr/bin/pkill", "-9", "-P", "$mainProcessPid", "-f", processName))
+    }
+
+    fun getSimulatorMainProcessPid(): Int {
+        val result = remote.execIgnoringErrors(listOf("/usr/bin/pgrep", "-fl", "launchd_sim"))
+
+        if (result.isSuccess) {
+            return parseSimulatorPid(result.stdOut)
+        }
+
+        val errorMessage = if (result.exitCode == 1) {
+            "No launchd_sim processes found on ${remote.publicHostName}. Result: $result"
+        } else {
+            "Failed to get process list for simulators at $deviceRef. StdErr: ${result.stdErr}"
+        }
+
+        logger.error(logMarker, errorMessage)
+        throw IllegalStateException(errorMessage)
+    }
+
+    private fun parseSimulatorPid(result: String): Int {
+        val simulatorProcess = result.lineSequence().firstOrNull { it.contains(udid) && !it.contains("pgrep") }
+
+        if (simulatorProcess != null) {
+            return Scanner(simulatorProcess).nextInt()
+        }
+
+        val errorMessage = "No launchd_sim process for $udid found on ${remote.publicHostName}. Result: $result"
+        logger.error(logMarker, errorMessage)
+        throw IllegalStateException(errorMessage)
     }
 }
