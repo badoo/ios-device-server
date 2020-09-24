ab00447737ef9c750b3d247f607ba4ac6ec33115
Vyacheslav Frolov <frolov78@gmail.com>
2020-09-24 10:33:36 +0100

    Increment simulator backup version

--- device-server/src/main/kotlin/com/badoo/automation/deviceserver/ios/simulator/backup/SimulatorBackup.kt
+++ device-server/src/main/kotlin/com/badoo/automation/deviceserver/ios/simulator/backup/SimulatorBackup.kt
@@ -35,7 +35,7 @@ class SimulatorBackup(
     ))
 
     companion object {
-        const val CURRENT_VERSION = 4
+        const val CURRENT_VERSION = 5
     }
 
     data class BackupMeta(val version: Int, val created: String) {
