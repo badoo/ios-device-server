2f664a579e0587d0524c3ea74daa108ae8ee50b5
Vyacheslav Frolov <frolov78@gmail.com>
2025-06-16 19:54:46 +0100

    Add check for occupied ports

--- device-server/src/main/kotlin/com/badoo/automation/deviceserver/host/management/PortAllocator.kt
+++ device-server/src/main/kotlin/com/badoo/automation/deviceserver/host/management/PortAllocator.kt
@@ -1,12 +1,13 @@
 package com.badoo.automation.deviceserver.host.management
 
 import com.badoo.automation.deviceserver.data.DeviceAllocatedPorts
+import com.badoo.automation.deviceserver.host.IRemote
 
-class PortAllocator(min: Int = PORT_RANGE_START, max: Int = PORT_RANGE_END) {
+class PortAllocator(private val remote: IRemote, min: Int = PORT_RANGE_START, max: Int = PORT_RANGE_END) {
     companion object {
         // this port range seems to be free and not in conflict with zabbix, etc.
         const val PORT_RANGE_START = 41798
-        const val PORT_RANGE_END = 42507
+        const val PORT_RANGE_END = 62507
     }
 
     private val ports: MutableSet<Int> = IntRange(min, max).toMutableSet()
@@ -27,11 +28,26 @@ class PortAllocator(min: Int = PORT_RANGE_START, max: Int = PORT_RANGE_END) {
     }
 
     private fun allocate(entries: Int): List<Int> {
-        //TODO: Check with "netstat -nat|grep LISTEN|grep -v tcp6" if ports are already occupied
         synchronized(this) {
+            val result = remote.shell("/usr/sbin/netstat -anv")
+
+            if (result.isSuccess) {
+                val spaceRegex = "\\s+".toRegex()
+                val occupiedPorts = result.stdOut.lines()
+                    .filter { it.contains("LISTEN") && it.trim().startsWith("tcp4") }
+                    .mapNotNull { line ->
+                        val parts = line.split(spaceRegex)
+                        parts.getOrNull(3)?.split(".")?.getOrNull(1)?.toIntOrNull()
+                    }
+                    .toSet()
+
+                ports.removeAll(occupiedPorts)
+            }
+
             if (ports.size < entries) {
                 throw RuntimeException("No more ports to allocate")
             }
+
             val takenPorts = ports.take(entries)
             ports.removeAll(takenPorts)
             return takenPorts
