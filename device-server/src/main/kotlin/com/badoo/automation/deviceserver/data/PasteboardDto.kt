62220ce55a5473c776323432970ca44ccde9e886
Vyacheslav Frolov <frolov78@gmail.com>
2025-06-21 18:27:17 +0100

    Generate equals and hashCode for Pasteboard

--- device-server/src/main/kotlin/com/badoo/automation/deviceserver/data/PasteboardDto.kt
+++ device-server/src/main/kotlin/com/badoo/automation/deviceserver/data/PasteboardDto.kt
@@ -5,4 +5,17 @@ import com.fasterxml.jackson.annotation.JsonProperty
 data class PasteboardDto(
     @JsonProperty("pasteboard_content")
     val pasteboardContent: ByteArray
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as PasteboardDto
+
+        return pasteboardContent.contentEquals(other.pasteboardContent)
+    }
+
+    override fun hashCode(): Int {
+        return pasteboardContent.contentHashCode()
+    }
+}
