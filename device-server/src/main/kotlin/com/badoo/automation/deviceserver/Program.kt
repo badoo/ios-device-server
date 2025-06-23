3a80256c8ec57d7100517ba971c98c4a1b9ca5d4
Vyacheslav Frolov <frolov78@gmail.com>
2025-06-24 00:18:45 +0100

    Use Ktor with CIO instead of Netty

--- device-server/src/main/kotlin/com/badoo/automation/deviceserver/Program.kt
+++ device-server/src/main/kotlin/com/badoo/automation/deviceserver/Program.kt
@@ -1,25 +1,10 @@
 package com.badoo.automation.deviceserver
 
-import io.ktor.server.netty.*
-
-//fun main(args: Array<String>) {
-//    embeddedServer(Netty, commandLineEnvironment(args)) {
-//        connectionGroupSize = Integer.getInteger("embedded.netty.connectionGroupSize", connectionGroupSize)
-//        workerGroupSize = Integer.getInteger("embedded.netty.workerGroupSize", workerGroupSize)
-//        callGroupSize = Integer.getInteger("embedded.netty.callGroupSize", callGroupSize)
-//    }.start(wait = true)
-//}
+import io.ktor.server.application.Application
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.cio.*
 
 fun main(args: Array<String>) {
-    System.setProperty("io.netty.eventLoopThreads", "200")
-    val server = EngineMain.createServer(args)
-    val workers = 300
-
-    with(server.engine.configuration) {
-        this.connectionGroupSize = workers
-        this.workerGroupSize = workers
-        this.callGroupSize = workers
-    }
-
-    server.start(wait = true)
+    embeddedServer(CIO, port = 4567, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
 }
