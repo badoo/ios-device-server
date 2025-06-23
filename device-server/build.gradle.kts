195d28d0557eb749b47b9b3ef8ee0abaad2de95e
Vyacheslav Frolov <frolov78@gmail.com>
2025-06-24 00:20:25 +0100

    Use Ktor with CIO instead Netty

--- device-server/build.gradle.kts
+++ device-server/build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
     implementation(libs.ktor.server.content.negotiation)
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.default.headers)
-    implementation(libs.ktor.server.netty)
+    implementation(libs.ktor.server.cio)
     implementation(libs.ktor.server.status.pages)
     implementation(libs.logback.classic)
 
