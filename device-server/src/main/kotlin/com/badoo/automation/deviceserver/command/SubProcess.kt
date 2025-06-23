b271da79e700a9ff398817521a46bf8676685bc1
Vyacheslav Frolov <frolov78@gmail.com>
2025-06-24 00:08:06 +0100

    Remove unused import

--- device-server/src/main/kotlin/com/badoo/automation/deviceserver/command/SubProcess.kt
+++ device-server/src/main/kotlin/com/badoo/automation/deviceserver/command/SubProcess.kt
@@ -9,7 +9,6 @@ import java.io.IOException
 import java.io.InputStream
 import java.io.InputStreamReader
 import java.nio.charset.StandardCharsets
-import java.util.concurrent.Callable
 import java.util.concurrent.Future
 import java.util.concurrent.FutureTask
 
@@ -87,4 +86,3 @@ class SubProcess private constructor(
         }
     }
 }
-
