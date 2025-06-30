008929388f2c75978508cf5481454d8fa85dd590
Vyacheslav Frolov <frolov78@gmail.com>
2025-06-30 15:53:47 +0100

    Remove backups in favor of simulator clones

--- device-server/src/main/kotlin/com/badoo/automation/deviceserver/ios/simulator/backup/ISimulatorBackup.kt
+++
@@ -1,8 +0,0 @@
-package com.badoo.automation.deviceserver.ios.simulator.backup
-
-interface ISimulatorBackup {
-    fun isExist(): Boolean
-    fun create()
-    fun restore()
-    fun delete()
-}
--- device-server/src/main/kotlin/com/badoo/automation/deviceserver/ios/simulator/backup/SimulatorBackupError.kt
+++
@@ -1,3 +0,0 @@
-package com.badoo.automation.deviceserver.ios.simulator.backup
-
-class SimulatorBackupError(message: String, cause: Throwable? = null) : RuntimeException(message, cause)
