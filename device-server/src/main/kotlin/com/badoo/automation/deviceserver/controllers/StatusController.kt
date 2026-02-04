a65d62ba5d19e6313c2445a18776429ab151b8f6
Vyacheslav Frolov <frolov78@gmail.com>
2026-02-04 23:12:43 +0000

    Remove unused code

--- device-server/src/main/kotlin/com/badoo/automation/deviceserver/controllers/StatusController.kt
+++ device-server/src/main/kotlin/com/badoo/automation/deviceserver/controllers/StatusController.kt
@@ -25,23 +25,7 @@ class StatusController(private val deviceManager: DeviceManager) {
     }
 
     private fun childHierarchy(route: Route?, path: String = "", margin: String = "\n. "): String {
-        // FIXME: Here should be routes hierarchy
+        // TODO: Implement route hierarchy display for welcome page
         return "FIXME: Here should be routes hierarchy"
-        //        val selector = route.selector.toString()
-        //        val maybe_real_endpoint =
-        //                if (!selector.startsWith("(method:"))
-        //                    ""
-        //                else
-        //                {
-        //                    val path_or_link =
-        //                            if (selector == "(method:GET)" && !path.contains('{'))
-        //                                "<a href='${path}'>$path</a>" // Safe to provide as a link: idempotent.
-        //                            else
-        //                                path
-        //                    "$margin $path_or_link ${route.selector}"
-        //                }
-        //        return maybe_real_endpoint +
-        //               route.children.sortedBy { it.toString() }
-        //                       .joinToString("" ) { childHierarchy(it, route.toString(), "$margin. ") }
     }
 }
