158647934bb26d3e103afa01f2042316ba4d9e01
Vyacheslav Frolov <frolov78@gmail.com>
2026-02-12 18:13:38 +0000

    Skip DTD fetch when read Info.plist files

--- device-server/src/main/kotlin/com/badoo/automation/deviceserver/util/InfoPlist.kt
+++ device-server/src/main/kotlin/com/badoo/automation/deviceserver/util/InfoPlist.kt
@@ -1,21 +1,19 @@
 package com.badoo.automation.deviceserver.util
 
 import org.apache.commons.configuration2.plist.XMLPropertyListConfiguration
-import java.io.BufferedReader
 import java.io.File
-import java.io.FileReader
 
 /**
  * Info.plist
  */
 class InfoPlist(file: File) {
-    private val config: XMLPropertyListConfiguration
+    private val config: XMLPropertyListConfiguration = XMLPropertyListConfiguration().apply {
+        // Read file and remove DOCTYPE declaration that causes DTD fetch
+        val xmlContent = file.readText()
+        val cleanedXml = xmlContent.replace(Regex("""<!DOCTYPE[^>]*>"""), "")
 
-    init {
-        config = XMLPropertyListConfiguration()
-        BufferedReader(FileReader(file)).use { input ->
-            config.read(input)
-        }
+        // Parse the cleaned XML (no external DTD fetch)
+        read(java.io.StringReader(cleanedXml))
     }
 
     fun bundleIdentifier(): String = config.getString("CFBundleIdentifier")
