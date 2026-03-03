8a6a789634e3a8ae9ee8506b6d1912dc5fb5fdac
Vyacheslav Frolov <frolov78@gmail.com>
2026-03-03 21:19:41 +0000

    Remove NodeRegistry, NodeWrapper, NodeRegistrar, NodeRestarter

--- device-server/src/main/kotlin/com/badoo/automation/deviceserver/host/management/IHostFactory.kt
+++
@@ -1,8 +0,0 @@
-package com.badoo.automation.deviceserver.host.management
-
-import com.badoo.automation.deviceserver.NodeConfig
-import com.badoo.automation.deviceserver.host.IDeviceNode
-
-interface IHostFactory {
-    fun getHostFromConfig(config: NodeConfig): IDeviceNode
-}
