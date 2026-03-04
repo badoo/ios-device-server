58b02497c0ca566e9019da39a926bedbb8330d21
Vyacheslav Frolov <frolov78@gmail.com>
2026-03-04 09:56:26 +0000

    Rewrite HostFactory to create nodes with a shared PortAllocator

--- device-server/src/main/kotlin/com/badoo/automation/deviceserver/host/HostFactory.kt
+++ device-server/src/main/kotlin/com/badoo/automation/deviceserver/host/HostFactory.kt
@@ -1,85 +1,72 @@
 package com.badoo.automation.deviceserver.host
 
 import com.badoo.automation.deviceserver.ApplicationConfiguration
-import com.badoo.automation.deviceserver.NodeConfig
-import com.badoo.automation.deviceserver.host.management.IHostFactory
+import com.badoo.automation.deviceserver.DeviceServerConfig
+import com.badoo.automation.deviceserver.host.management.PortAllocator
 import com.badoo.automation.deviceserver.host.management.SimulatorHostChecker
+import com.badoo.automation.deviceserver.util.NetworkUtils
 import com.badoo.automation.deviceserver.util.WdaDeviceBundle
 import com.badoo.automation.deviceserver.util.WdaDeviceBundlesProvider
 import com.badoo.automation.deviceserver.util.WdaSimulatorBundles
 import com.badoo.automation.deviceserver.util.WdaSimulatorBundlesProvider
 import org.slf4j.LoggerFactory
-import java.io.File
 import java.nio.file.Paths
 
 class HostFactory(
     private val remoteProvider: (hostName: String, publicHost: String) -> IRemote = { hostName, publicHostName ->
-        Remote(
-            hostName,
-            publicHostName
-        )
+        Remote(hostName, publicHostName)
     },
-    private val remoteTestHelperAppRoot: File,
     private val appConfiguration: ApplicationConfiguration
-) : IHostFactory {
+) {
     private val logger = LoggerFactory.getLogger(javaClass.simpleName)
 
-    fun  getWdaDeviceBundles(appConfiguration: ApplicationConfiguration): List<WdaDeviceBundle> {
-        val wdaDeviceBundles: List<WdaDeviceBundle> = WdaDeviceBundlesProvider(
-            Paths.get(appConfiguration.wdaDeviceBundles),
-            Paths.get(appConfiguration.remoteWdaDeviceBundleRoot)
-        ).getWdaDeviceBundles()
+    fun createNodes(config: DeviceServerConfig): List<IDeviceNode> {
+        val publicHostName = config.publicHostName ?: NetworkUtils.getAddresses().first().ip
+        val remote = remoteProvider(publicHostName, publicHostName)
+        val sharedPortAllocator = PortAllocator(remote)
+        val nodes = mutableListOf<IDeviceNode>()
 
-        return wdaDeviceBundles
+        config.simulators?.let { sim ->
+            logger.info("Creating SimulatorsNode for $publicHostName (limit=${sim.simulatorLimit})")
+            nodes += SimulatorsNode(
+                remote = remote,
+                publicHostName = publicHostName,
+                hostChecker = SimulatorHostChecker(remote, sim.shutdownSimulators),
+                simulatorLimit = sim.simulatorLimit,
+                concurrentBoots = sim.concurrentBoots,
+                wdaSimulatorBundles = getWdaSimulatorBundles(),
+                portAllocator = sharedPortAllocator,
+                disabledServices = sim.disabledSimulatorServices
+            )
+        }
+
+        config.devices?.let { dev ->
+            logger.info("Creating DevicesNode for $publicHostName")
+            nodes += DevicesNode(
+                remote = remote,
+                publicHostName = publicHostName,
+                portAllocator = sharedPortAllocator,
+                configuredDevices = dev.configuredDevices,
+                whitelistedApps = dev.whitelistApps,
+                uninstallApps = dev.uninstallApps,
+                wdaDeviceBundles = getWdaDeviceBundles()
+            )
+        }
+
+        return nodes
     }
 
-    fun getWdaSimulatorBundles(appConfiguration: ApplicationConfiguration): WdaSimulatorBundles {
-        val wdaSimulatorBundles: WdaSimulatorBundles = WdaSimulatorBundlesProvider(
+    private fun getWdaSimulatorBundles(): WdaSimulatorBundles {
+        return WdaSimulatorBundlesProvider(
             Paths.get(appConfiguration.wdaSimulatorBundles),
             Paths.get(appConfiguration.remoteWdaSimulatorBundleRoot)
         ).getWdaSimulatorBundles()
-
-        return wdaSimulatorBundles
     }
 
-    override fun getHostFromConfig(config: NodeConfig): IDeviceNode {
-        logger.info("Trying to start node $config.")
-
-        val hostName = config.host
-        val publicHostName = config.publicHost
-        val remote: IRemote = remoteProvider(hostName, publicHostName)
-
-        val nodeTypeResult = remote.exec(listOf("/usr/bin/arch"), mapOf(),true, 60)
-        if (nodeTypeResult.isSuccess) {
-            logger.info("ARCH: The executor arch is ${nodeTypeResult.stdOut} for node $publicHostName")
-        } else {
-            logger.error("ARCH: Failed to determine executor type for node $publicHostName. (maybe it's Linux). ${nodeTypeResult.stdErr}")
-        }
-
-        return if (config.type == NodeConfig.NodeType.Simulators) {
-            val wdaSimulatorBundles = getWdaSimulatorBundles(appConfiguration)
-            val hostChecker = SimulatorHostChecker(
-                remote,
-                shutdownSimulators = config.shutdownSimulators,
-            )
-            SimulatorsNode(
-                remote = remote,
-                publicHostName = publicHostName,
-                hostChecker = hostChecker,
-                simulatorLimit = config.simulatorLimit,
-                concurrentBoots = config.concurrentBoots,
-                wdaSimulatorBundles = wdaSimulatorBundles,
-                disabledServices = config.disabledSimulatorServices
-            )
-        } else {
-            DevicesNode(
-                remote = remote,
-                publicHostName = publicHostName,
-                whitelistedApps = config.whitelistApps,
-                configuredDevices = config.configuredDevices,
-                uninstallApps = config.uninstallApps,
-                wdaDeviceBundles = getWdaDeviceBundles(appConfiguration),
-            )
-        }
+    private fun getWdaDeviceBundles(): List<WdaDeviceBundle> {
+        return WdaDeviceBundlesProvider(
+            Paths.get(appConfiguration.wdaDeviceBundles),
+            Paths.get(appConfiguration.remoteWdaDeviceBundleRoot)
+        ).getWdaDeviceBundles()
     }
 }
